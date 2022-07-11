bd8de17294c75bba39b12ce72db93d752d79d310
jgyselov <jgyselov@redhat.com>
2022-07-11 10:54:25 +0200

    Add asserts to dual-stack networking page tests

--- packages/integration-tests/src/integration/dualstack/2-networking.spec.js
+++ packages/integration-tests/src/integration/dualstack/2-networking.spec.js
@@ -18,25 +18,44 @@ describe(`Assisted Installer Dualstack Networking`, () => {
 
   afterEach(() => {
     commonActions.waitForSave();
+    commonActions.getNextButton().should('be.enabled');
+  });
+
+  after('Should go to the final step', () => {
+    commonActions.clickNextButton();
   });
 
   it('Can switch to dual-stack networking type', () => {
-    networkingPage.getStackTypeSingleStack().should('be.enabled').should('be.checked');
+    networkingPage.getStackTypeSingleStack().should('be.enabled').and('be.checked');
     networkingPage.getStackTypeDualStack().should('be.enabled').check();
 
-    Cypress.env('TRANSFORM_SIGNAL', 'dual-stack');
-    cy.wait('@update-cluster');
+    if (utils.isAIAPIMocked) {
+      Cypress.env('TRANSFORM_SIGNAL', 'dual-stack');
+      cy.wait('@update-cluster');
+    }
+
+
+    networkingPage.getClusterManagedNetworking().should('be.disabled').and('be.checked');
+    networkingPage.getStackTypeDualStack().should('be.enabled').and('be.checked');
+    networkingPage.getVipDhcp().should('be.disabled').and('not.be.checked');
+    networkingPage.getOvnNetworkingField().should('be.enabled').and('be.checked');
+    networkingPage.getSdnNetworkingField().should('be.disabled');
   });
 
   it('Can switch to IPv4 networking type', () => {
-    networkingPage.getStackTypeDualStack().should('be.enabled').should('be.checked');
+    networkingPage.getStackTypeDualStack().should('be.enabled').and('be.checked');
     networkingPage.getStackTypeSingleStack().should('be.enabled').check();
     networkingPage.confirmStackTypeChange();
-    Cypress.env('TRANSFORM_SIGNAL', 'single-stack');
-    cy.wait('@update-cluster');
+
+    if (utils.isAIAPIMocked()) {
+      Cypress.env('TRANSFORM_SIGNAL', 'single-stack');
+      cy.wait('@update-cluster');
+    }
+
+    networkingPage.getClusterManagedNetworking().should('be.enabled').and('be.checked');
+    networkingPage.getStackTypeSingleStack().should('be.enabled').and('be.checked');
+    networkingPage.getVipDhcp().should('be.enabled').and('be.checked');
+    networkingPage.getSdnNetworkingField().should('be.enabled').and('be.checked');
   });
 
-  it('Should go to the final step', () => {
-    commonActions.clickNextButton();
-  });
 });
