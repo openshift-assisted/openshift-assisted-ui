ce7fd754d70ead4e6868d2171d1e9c56451270b7
Julie Gyselová <jgyselov@redhat.com>
2022-12-21 14:16:35 +0100

    Adaptations for the 'Review and create' redesign (#25)

--- packages/integration-tests/src/support/variables/review-create.ts
+++ packages/integration-tests/src/support/variables/review-create.ts
@@ -1,8 +1,13 @@
 // Review and Create
+
+// validations
+Cypress.env('clusterValidations', '.review-expandable');
 Cypress.env('clusterValidationsValueId', '[data-testid=cluster-validations-value]');
 Cypress.env('hostValidationsValueId', '[data-testid=host-validations-value]');
-Cypress.env('clusterAddressValueId', '[data-testid=cluster-address-value]');
-Cypress.env('openshiftVersionValueId', '[data-testid=openshift-version-value]');
 Cypress.env('allValidationsPassedText', 'All validations passed');
-Cypress.env('stackTypeValueId', '[data-testid="stack-type-value"]');
-Cypress.env('networkCidrValueId', '[data-testid="network-cidr-value"]');
+
+// summary
+Cypress.env('clusterAddressValueId', '[data-testid=cluster-address]');
+Cypress.env('openshiftVersionValueId', '[data-testid=openshift-version]');
+Cypress.env('stackTypeValueId', '[data-testid="stack-type"]');
+Cypress.env('machineNetworksValueId', '[data-testid="machine-networks"]');
