8a33b11e84aeddebe410d47137c972f9bde1aa3e
Julie Gyselová <jgyselov@redhat.com>
2023-03-23 09:23:54 +0100

    Test adaptations for 2.17 (#30)

--- packages/integration-tests/src/integration/create-sno/2-networking.spec.ts
+++ packages/integration-tests/src/integration/create-sno/2-networking.spec.ts
@@ -35,7 +35,7 @@ describe(`Assisted Installer SNO Networking`, () => {
     it('Should have the correct default network type', () => {
       networkingPage.getAdvancedNetwork().click();
       networkingPage.getSdnNetworkingField().should('not.be.enabled').and('not.be.checked');
-      networkingPage.getOvnNetworkingField().should('be.enabled').and('be.checked');
+      networkingPage.getOvnNetworkingField().should('not.be.enabled').and('be.checked');
     });
 
     it('Should go to the final step', () => {
